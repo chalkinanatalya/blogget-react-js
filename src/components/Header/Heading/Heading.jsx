c5936a835376061d0561b1fbce995f2edcf1303f
Natalya <m_d.r.lecter@protonmail.com>
2023-09-16 19:11:57 +0400

    fix

--- src/components/Header/Heading/Heading.jsx
+++ src/components/Header/Heading/Heading.jsx
@@ -1,7 +1,4 @@
-import style from './Heading.module.css';
 
-export const Heading = (props) => {
-  return (
-    <h1>{props.text}</h1>
-  );
-}
+export const Heading = (props) => (
+  <h1>{props.text}</h1>
+);
