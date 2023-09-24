f03e3a138dc7082a081b558eca7cd939fdeb80a9
Natalya <m_d.r.lecter@protonmail.com>
2023-09-24 22:08:59 +0400

    menu, text component

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,17 +2,50 @@ import style from './List.module.css';
 import {Post} from './Post/Post';
 
 export const List = () => {
-  const postData = {
-    thumbnailUrl: '',
-    title: 'Title',
-    author: 'Nickname',
-    ups: 24,
-    date: '2023-08-24T09:45:00.000Z',
-  };
+  const postsData = [
+    {
+      id: '34',
+      thumbnailUrl: '',
+      title: 'Title1',
+      author: 'Nickname1',
+      ups: 77,
+      date: '2023-08-24T09:45:00.000Z',
+    },
+
+    {
+      id: '45',
+      thumbnailUrl: '',
+      title: 'Title2',
+      author: 'Nickname2',
+      ups: 58,
+      date: '2023-04-20T09:45:00.000Z',
+    },
+
+    {
+      id: '67',
+      thumbnailUrl: '',
+      title: 'Title3',
+      author: 'Nickname3',
+      ups: 24,
+      date: '2023-02-18T00:45:00.000Z',
+    },
+
+    {
+      id: '78',
+      thumbnailUrl: '',
+      title: 'Title4',
+      author: 'Nickname4',
+      ups: 124,
+      date: '2023-03-11T08:15:00.000Z',
+    },
+  ];
 
   return (
     <ul className={style.list}>
-      <Post postData={postData} />
+      {
+        postsData.map((postData) => <Post key={postData.id} postData={postData} />)
+      }
     </ul>
   );
 };
+
--- src/components/Main/List/Post/Rating/Rating.jsx
+++ src/components/Main/List/Post/Rating/Rating.jsx
@@ -1,16 +1,13 @@
 import style from './Rating.module.css';
 import PropTypes from 'prop-types';
 
-export const Rating = ({ups}) => {
-  console.log(ups);
-  return (
-    <div className={style.rating}>
-      <button className={style.up} aria-label="увеличить рейтинг"/>
-      <p className={style.ups}>{ups}</p>
-      <button className={style.down} aria-label="понизить рейтинг"/>
-    </div>
-  );
-};
+export const Rating = ({ups}) => (
+  <div className={style.rating}>
+    <button className={style.up} aria-label="увеличить рейтинг"/>
+    <p className={style.ups}>{ups}</p>
+    <button className={style.down} aria-label="понизить рейтинг"/>
+  </div>
+);
 
 Rating.propTypes = {
   ups: PropTypes.number,
