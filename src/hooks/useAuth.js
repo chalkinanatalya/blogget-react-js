469d09166f8527862f47a52693b4390574d4cbc5
Natalya <m_d.r.lecter@protonmail.com>
2023-09-29 19:59:28 +0400

    logout, useAuth

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,26 @@
+import {useEffect, useState} from 'react';
+import {URL_API} from '../api/const';
+
+export const useAuth = (token) => {
+  const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    if (!token) return;
+    fetch(`${URL_API}/api/v1/me`, {
+      headers: {
+        Authorization: `bearer ${token}`
+      },
+    }).then(response => response.json()
+      .then(({name, icon_img: iconImg}) => {
+        const img = iconImg.replace(/\?.*$/, '');
+        setAuth({name, img});
+      }
+      ))
+      .catch(err => {
+        console.error(err);
+        setAuth({});
+      });
+  }, [token]);
+
+  return auth;
+};
