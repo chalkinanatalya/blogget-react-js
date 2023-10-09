896d18d8e106c245ada709940ac7ec14f4e6bd5a
Natalya <m_d.r.lecter@protonmail.com>
2023-10-09 20:04:09 +0400

    del contexts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import {Header} from './components/Header/Header';
 import {Main} from './components/Main/Main';
+import {Provider} from 'react-redux';
 import {AuthContextProvider} from './context/authContext';
 import {PostsContextProvider} from './context/postContext';
 import {TokenContextProvider} from './context/tokenContext';
+import {store} from './store';
 
 function App() {
   return (
-    <TokenContextProvider>
-      <AuthContextProvider>
-        <PostsContextProvider>
-          <Header />
-          <Main />
-        </PostsContextProvider>
-      </AuthContextProvider>
-    </TokenContextProvider>
+    <Provider store={store}>
+      <TokenContextProvider>
+        <AuthContextProvider>
+          <PostsContextProvider>
+            <Header />
+            <Main />
+          </PostsContextProvider>
+        </AuthContextProvider>
+      </TokenContextProvider>
+    </Provider>
   );
 }
 
