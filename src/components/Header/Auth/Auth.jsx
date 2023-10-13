4b5d9482674438536b022d462b177b98136cea88
Natalya <m_d.r.lecter@protonmail.com>
2023-10-13 21:07:17 +0400

    combine reducer and fix

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -5,11 +5,13 @@ import style from './Auth.module.css';
 import {ReactComponent as LoginIcon} from './img/login.svg';
 import PropTypes from 'prop-types';
 import {authContext} from '../../../context/authContext';
-import {useDispatch} from 'react-redux';
-import {deleteToken} from '../../../store';
+import {useDispatch, useSelector} from 'react-redux';
+import {deleteToken} from '../../../store/tokenReducer';
+
 
 export const Auth = () => {
   const [showLogout, setShowLogout] = useState(false);
+  const token = useSelector(state => state.token.token);
   const {auth, clearAuth} = useContext(authContext);
   const dispatch = useDispatch();
 
@@ -24,7 +26,7 @@ export const Auth = () => {
 
   return (
     <div className={style.container}>
-      {auth.name ? (
+      {token && auth.name ? (
         <>
           <button className={style.btn}>
             <img className={style.img} src={auth.img} title={`Аватар ${auth.name}`} onClick={toggleLogoutButton}/>
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,11 @@
 import {useEffect, useState} from 'react';
 import {URL_API} from '../api/const';
 import {useDispatch, useSelector} from 'react-redux';
-import {deleteToken} from '../store';
+import {deleteToken} from '../store/tokenReducer';
 
 export const useAuth = () => {
   const [auth, setAuth] = useState({});
-  const token = useSelector(state => state.token);
+  const token = useSelector(state => state.token.token);
   const dispatch = useDispatch();
 
   useEffect(() => {
