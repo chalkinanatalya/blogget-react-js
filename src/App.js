595c8b0cefa7368bda02c6de5b09aaceedff4ec8
Natalya <m_d.r.lecter@protonmail.com>
2023-10-15 18:00:42 +0400

    middleware

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import {Header} from './components/Header/Header';
 import {Main} from './components/Main/Main';
-import {Provider} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {AuthContextProvider} from './context/authContext';
 import {PostsContextProvider} from './context/postContext';
-import {store} from './store';
+import {getToken} from './api/token';
+import {updateToken} from './store/tokenReducer';
 
 function App() {
+  const dispatch = useDispatch();
+  dispatch(updateToken(getToken()));
   return (
-    <Provider store={store}>
-      <AuthContextProvider>
-        <PostsContextProvider>
-          <Header />
-          <Main />
-        </PostsContextProvider>
-      </AuthContextProvider>
-    </Provider>
+    <AuthContextProvider>
+      <PostsContextProvider>
+        <Header />
+        <Main />
+      </PostsContextProvider>
+    </AuthContextProvider>
   );
 }
 
